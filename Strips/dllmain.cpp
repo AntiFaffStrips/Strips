98792b21f3f850c35526e652ab5dd641ddeb8dd1
Jonathan Ferguson <jonathan.ferguson.05@gmail.com>
2020-04-02 19:22:19 +0100

    Temp Save

--- Strips/dllmain.cpp
+++ Strips/dllmain.cpp
@@ -1,6 +1,5 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
-#include "EuroScopePlugIn.h"
 #include "CEuroScopeUtils.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
