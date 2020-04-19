76096274870076dcaf568d05d5cf7065b4212051
1adamf <adam.farq@icloud.com>
2020-04-19 18:39:20 +0100

    Create core CController class. Error checking and some features still to be implemented

--- Strips/CController.cpp
+++ Strips/CController.cpp
@@ -1,2 +1,44 @@
 #include "pch.h"
 #include "CController.h"
+
+/*******************************************************************************
+* Class constructor
+*
+* Accepts Callsign of the controllers position
+
+* Throws (add what it throws)
+*/
+CController::CController(string callsign) {
+	//checking inputs
+	if (callsign.find_first_of("_") == -1) {
+		throw "The callsign entered in the CController constructor was invalid(didn't contain an underscore";
+	}
+
+	//Think of more potential errors, maybe use machine
+
+	//Assigning instance variables
+	CPosition position = positionFromCallsign(callsign);
+	m_position = position;
+	m_callsign = callsign;
+}
+
+CPosition CController::getPosition() {
+	return m_position;
+}
+
+string CController::getCallsign() {
+	return m_callsign;
+}
+
+bool CController::updatePosition(CPosition newPosition) {
+	//check inputs
+	m_position = newPosition;
+}
+
+//Static methods
+
+CPosition CController::positionFromCallsign(string callsign) {
+	//find what adjacent controllers are online
+	//work out position based on that
+	//If airport has specific position and not generic then use that
+}
--- Strips/CController.h
+++ Strips/CController.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <string>
+#include "CPosition.h"
+using namespace std;
 class CController
 {
+public:
+	CController(string callsign);
+	CPosition getPosition();
+	string getCallsign();
+	bool updatePosition(CPosition newPosition);
+	static CPosition positionFromCallsign(string callsign);
+private:
+	CPosition m_position;
+	string m_callsign;
 };
-
