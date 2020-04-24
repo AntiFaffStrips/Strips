f142a1d76522d401801f2ac1833a82731700be00
1adamf <adam.farq@icloud.com>
2020-04-24 01:05:45 +0100

    Declare update methods

--- Strips/CPosition.h
+++ Strips/CPosition.h
@@ -8,8 +8,10 @@
 class CPosition
 {
 public:
+	//class constructor
+	CPosition(std::string ICAO, std::list<CBox> boxes, std::list<std::string> adjacentControllers, char atis, int runway, std::unordered_map<std::string, bool> sids, int qnh, std::string callsignSelected); //needs issue update
+
 	//get methods
-	CPosition(std::string ICAO, std::list<CBox> boxes, std::list<std::string> adjacentControllers, char atis, int runway, std::unordered_map<std::string, bool> sids, int qnh, std::string callsignSelected);
 	std::string getICAO();
 	std::list<CBox> getBoxes();
 	std::list<std::string> getAdjacentControllers();
@@ -19,5 +21,14 @@ public:
 	std::string getCallsignSelected();
 
 	//update methods
+	void updateAdjacentControllers(std::unordered_map < std::list<std::string>, std::list<std::string>> updateInfo); //needs issue update
+	void updateAtis(char newAtis);
+	void updateAtis(int change);
+	void updateRunway(int newRunway);
+	void updateQNH(int newQNH);//needs issue iupdate
+	void updateCallsignSelected(std::string newCallsign);
 
+	//other instance methods - (non at the moment but leaving space to add if needed)
+
+	//static methods - (non at the moment but leaving space to add if needed)
 };
