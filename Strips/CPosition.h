98049e5c02a9e4ef6735d12a1642d6d29e8a3094
1adamf <adam.farq@icloud.com>
2020-04-24 02:18:38 +0100

    Add class constructor documentation

--- Strips/CPosition.h
+++ Strips/CPosition.h
@@ -14,7 +14,20 @@
 class CPosition
 {
 public:
-	//class constructor
+	/**
+	* Class constructor
+	*
+	* @param string with the ICAO of the airport the being controlled - Must start with "EG" (be in the uk)
+	* @param list of CBox objects with the boxes that are required for this position - The list must not be null and must not be empty.
+	* @param list of adjacent controllers that are currently online - each callsign in the list must be valid(callsign must contain at least 1 underscore, start with "EG" and have an extension after the last underscore of any of the following: "DEL", "GND", "TWR", "APP", "CTR") and the list must not be null or emtpy.
+	* @param character with the current atis information - character must be uppercase and be in the range A-Z inclusive. and cannot be null
+	* @param integer with the current runway in use - integer must not be null and must be in the range 1-36 inclusive
+	* @param unordered map which maps a string with the sid to a boolean which is true if freeflow and false otherwise - each sid must be a valid sid for that aiport(should be achieved by only loading sids from the approved .xml files and an incorrect sid will not throw and exception), map cannot be null nor can any sid in the map or the mapped booolean value.
+	* @param integer with the current QNH - Must not be null and vlaue must be in the range 931-1067
+	* @param string with the callsign selected - must not be null and must have an associated CStrip object
+	* 
+	* @throws exception if any of the inputs are invalid. Details on what is valid above
+	*/
 	CPosition(std::string ICAO, std::list<CBox> boxes, std::list<std::string> adjacentControllers, char atis, int runway, std::unordered_map<std::string, bool> sids, int qnh, std::string callsignSelected); //needs issue update
 
 	//get methods
@@ -37,7 +50,9 @@ public:
 	//other instance methods - (non at the moment but leaving space to add if needed)
 
 	//public static methods - (non at the moment but leaving space to add if needed)
+
 private:
+	//member variables
 	std::string ICAO;
 	std::list<CBox> boxes;
 	std::list<std::string> adjacentControllers;
